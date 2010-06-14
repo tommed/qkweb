f3a4416992a9517f52ee9d81af8f6e94043a8597
Tom Medhurst <tom.medhurst@gmail.com>
2010-06-14 18:51:00 +0100

    scaffold placeholders added

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import cgitb
+from qkweb.scaffold import ScaffoldHandler
 
 class Runner:
 	"""used to start a derrivative of BaseHandler"""
@@ -8,6 +9,7 @@ class Runner:
 		cgitb.enable()
 		handler.run()
 
+	@staticmethod
 	def scaffold(klass, db, model, base_template=None, view_dir='.'):
 		cgitb.enable()
 		ScaffoldHandler(klass, db, model, view_dir=view_dir).run()
--- scaffold.py
+++ scaffold.py
@@ -13,54 +13,53 @@ class ScaffoldHandler(BaseHandler):
 		self.klass = klass
 
 	def index_(self, form):
-		self.set_mime('text/html')
 		print "TODO! list all items"
 
 	def read_(self, id):
-		self.set_mime('text/html')
 		print "TODO! read item %d" % id
 
 	def update_form_(self, id):
-		self.set_mime('text/html')
 		print "TODO! draw update form item %d" % id
 
 	def create_form_(self):
-		self.set_mime('text/html')
 		print "TODO! draw create form"
 
 	def update_(self, form):
-		self.set_mime('text/html')
 		id = int(form.getvalue('id'))
 		print "TODO! update item %d" % id
 
 	def create_(self, form):
-		self.set_mime('text/html')
 		print "TODO! create new item"
 
 	def delete_(self, id):
-		self.set_mime('text/html')
 		print "TODO! delete item %d" % id
 
 	def GET(self, form):
+		self.set_mime("text/html")
 		if 'id' not in form and 'a' not in form:
-			index_(self, form)
+			self.index_(form)
 		elif 'a' not in form:
-			read_(self, int(form.getvalue('id')))
+			self.read_(int(form.getvalue('id')))
 		elif form.getvalue('a') == 'update':
-			update_form_(self, int(form.getvalue('id')))
+			self.update_form_(int(form.getvalue('id')))
 		elif form.getvalue('a') == 'create':
-			create_form_(self)
+			self.create_form_()
+		else:
+			print "NOT IMPLEMENTED %s" % form.getvalue('a')
 
 	def POST(self, form):
+		self.set_mime("text/html")
 		if 'id' not in form and 'a' not in form:
-			create_(self, form)
+			self.create_(form)
 		elif 'a' not in form:
-			update_(self, form)
+			self.update_(form)
 		elif form.getvalue('a') == 'update':
-			update_(self, form)
+			self.update_(form)
 		elif form.getvalue('a') == 'create':
-			create_(self, form)
+			self.create_(form)
 		elif form.getvalue('a') == 'delete':
-			delete_(self, int(form.getvalue('id')))
+			self.delete_(int(form.getvalue('id')))
+		else:
+			print "NOT IMPLEMENTED %s" % form.getvalue('a')
 
 
